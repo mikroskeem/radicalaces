5e659b4ef0662621c7a42f13c8b64a53769872ea
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2019-03-07 23:37:08 +0200

    Add option to use ECJ instead of javac
    ECJ does not require JDK installation
    Use with 'gradle -PuseEcj`

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,34 @@ plugins {
 
 group = "eu.mikroskeem"
 
+val ecjVersion = "3.16.0"
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+val compileJava by tasks.getting(JavaCompile::class) {
+    if(project.hasProperty("useEcj")) {
+        println("*******************************")
+        println("* Using Eclipse Java Compiler *")
+        println("*******************************")
+
+        project.configurations.maybeCreate("ecj")
+        project.dependencies.add("ecj", "org.eclipse.jdt:ecj:$ecjVersion")
+
+        options.isFork = true
+        options.forkOptions.apply {
+            executable = "java"
+            jvmArgs = listOf(
+                    "-classpath",
+                    project.configurations["ecj"].asPath,
+                    "org.eclipse.jdt.internal.compiler.batch.Main"
+            )
+        }
+    }
+}
+
 val jar by tasks.getting(Jar::class) {
     manifest {
         attributes(
